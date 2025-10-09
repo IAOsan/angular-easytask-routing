5f6c024927eb355f2bf14747db8d3958add5900d
Yaotzin Sanabria <yaosanabria@gmail.com>
2025-10-09 02:19:27 -0600

    feat(routing): add static and dynamic titles to routes and centralize site constants

--- src/app/tasks/userName.resolver.ts
+++ src/app/tasks/userName.resolver.ts
@@ -1,13 +1,36 @@
 import { inject, Type } from '@angular/core';
-import { ActivatedRouteSnapshot, ResolveFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterState,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { UsersService } from '../users/users.service';
+import { SITE_NAME } from '../app.constants';
+
+function getUser(route: ActivatedRouteSnapshot) {
+  const usersService = inject(UsersService as Type<UsersService>);
+  const userId = route.paramMap.get('userId') || '';
+  return usersService.getUserById(userId);
+}
 
 export const userNameResolveFn: ResolveFn<string> = (
   route: ActivatedRouteSnapshot
 ) => {
-  const usersService = inject(UsersService as Type<UsersService>);
-  const userId = route.paramMap.get('userId') || '';
-  const user = usersService.getUserById(userId);
+  const user = getUser(route);
 
   return user?.name || '';
+};
+
+export const titleResolveFn: ResolveFn<string> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const user = getUser(route);
+
+  if (user) {
+    return `${SITE_NAME} | ${user.name}'s tasks`;
+  }
+
+  return `${SITE_NAME} | user tasks`;
 };
